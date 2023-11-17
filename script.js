23ca21edaf6193c2123f716cce2952785f9f2289
fatmaa1601 <fatmaa1601@gmail.com>
2023-11-17 13:44:37 +0200

    add main files

---
+++ script.js
@@ -0,0 +1,9 @@
+let Popup = document.querySelector(".Popup");
+
+function OpenPopup() {
+  Popup.classList.add("open-Popup");
+}
+
+function ClosePopup() {
+  Popup.classList.remove("open-Popup");
+}
